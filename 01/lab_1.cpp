5ae6fcfdc7bb9f142b5f7a08cda9defa6b1daaf8
MMotionMan <114028585+KirichenkoAV22VT191@users.noreply.github.com>
2023-11-05 16:46:30 +0300

    Update lab_1.cpp
    Добавил комментарии
    Обработал передачу 0 в метод alloc.

--- 01/lab_1.cpp
+++ 01/lab_1.cpp
@@ -2,12 +2,17 @@
 #include "lab_1.h"
 
 
+// Конструкто
 Allocator::Allocator() {
     start_pointer = nullptr;
     offset = 0;
     full_size = 0;
 }
 
+
+// Метод для создания аллокатора и выделения maxSize бит памяти.
+// input: maxSize - кол-во памяти выделяемой при создании аллокатор.
+// При передаче maxSize = 0, start_pointer будет присвоен nullptr
 void Allocator::makeAllocator(size_t maxSize) {
     if (start_pointer != nullptr) {
         delete[] start_pointer;
@@ -24,8 +29,14 @@ void Allocator::makeAllocator(size_t maxSize) {
 
 }
 
+// Метод для выделения блока памяти размером size в аллокаторе.
+// input: size - размер блока памяти для выделения.
+// return: указатель на созданный блок памяти в аллокаторе, либо nullptr, если памяти для выделения не достаточно, или
+// size = 0.
 char *Allocator::alloc(size_t size) {
-    if (offset + size <= full_size) {
+    if (size == 0) {
+        return nullptr;
+    } else if (offset + size <= full_size) {
         char *new_block = start_pointer + offset;
         offset = offset + size;
         return new_block;
@@ -34,9 +45,13 @@ char *Allocator::alloc(size_t size) {
     return nullptr;
 }
 
+//Метод для оичистки занятой памяти в аллокаторе
+
 void Allocator::reset() {
     offset = 0;
 }
+
+//Деструктор
 
 Allocator::~Allocator() {
     delete[] start_pointer;
